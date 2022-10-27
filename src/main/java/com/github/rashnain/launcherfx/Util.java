2853b9cde0645963eb17f07732d4fc40123bbda9
Rashnain <51191602+Rashnain@users.noreply.github.com>
2022-10-27 22:41:20 +0200

    Created loadJSON in Util

--- src/main/java/com/github/rashnain/launcherfx/Util.java
+++ src/main/java/com/github/rashnain/launcherfx/Util.java
@@ -1,6 +1,7 @@
 package main.java.com.github.rashnain.launcherfx;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
@@ -18,7 +19,7 @@ import javafx.scene.Node;
 
 public class Util {
 	
-	public static final String MANIFEST_VERIONS = "https://piston-meta.mojang.com/mc/game/version_manifest_v2.json";
+	public static final String VERSION_MANIFEST = "https://piston-meta.mojang.com/mc/game/version_manifest_v2.json";
 
 	/**
      * Download and return the JsonElement specified by the url parameter
@@ -36,9 +37,17 @@ public class Util {
         StringBuilder jsonSB = new StringBuilder();
         int size;//, subSize;
 
+        
         while ((size=stream.read(buffer)) > 0) {
         	if (size != buffer.length) {
-        		subBuffer = new byte[size]; // -1 is to ignore the end of file byte
+        		//System.out.println(buffer[size-1]);
+        		//System.out.println(buffer[size]);
+        		//if (buffer[size] == -1) {
+        		//	subSize = size-1;
+        		//} else {
+        		//	subSize = size;
+        		//}
+        		subBuffer = new byte[size];
         		for (int i = 0; i < size; i++) { 
 					subBuffer[i] = buffer[i];
 				}
@@ -66,14 +75,14 @@ public class Util {
 	 * Download file at URL if path/filename doesn't exists
 	 * @param URL
 	 * @param filename
-	 * @param path
+	 * @param dir
 	 * @throws IOException 
 	 */
-	public static void downloadFile(String URL, String filename, String path, int size) throws IOException {
-		System.out.println(path+filename);
-		File pathFile = new File(path);
+	public static void downloadFile(String URL, String filename, String dir, int size) throws IOException {
+		System.out.println("Downloading " + filename + " to " + dir);
+		File pathFile = new File(dir);
 		pathFile.mkdirs();
-		File file = new File(path+filename);
+		File file = new File(dir+filename);
 		if (file.createNewFile()) {// || file.getTotalSpace() != size) {
 			URLConnection conn = new URL(URL).openConnection();
 			conn.setRequestProperty("User-Agent", "Wget/1.21.3 (linux-gnu)");
@@ -87,7 +96,7 @@ public class Util {
 
 	        while ((sizeBuffer=in.read(buffer)) > 0) {
 	        	if (sizeBuffer != buffer.length) {
-	        		subBuffer = new byte[sizeBuffer]; // -1 is to ignore the end of file byte
+	        		subBuffer = new byte[sizeBuffer];
 	        		for (int i = 0; i < sizeBuffer; i++) { 
 						subBuffer[i] = buffer[i];
 					}
@@ -100,6 +109,32 @@ public class Util {
 	        
 	        out.close();
 		}
+	}
+
+	public static JsonElement loadJSON(String path) throws IOException {
+		FileInputStream in = new FileInputStream(path);
+		StringBuilder sb = new StringBuilder();
+		
+		byte[] buffer = new byte[4048];
+        byte[] subBuffer;
+        int sizeBuffer;
+
+        while ((sizeBuffer=in.read(buffer)) > 0) {
+        	if (sizeBuffer != buffer.length) {
+        		subBuffer = new byte[sizeBuffer];
+        		for (int i = 0; i < sizeBuffer; i++) { 
+					subBuffer[i] = buffer[i];
+				}
+        		sb.append(new String(subBuffer));
+        	} else {
+        		sb.append(new String(buffer));
+        	}
+        	buffer = new byte[1024];
+        }
+        
+        in.close();
+        
+		return JsonParser.parseString(sb.toString());
 	}
 
 }
