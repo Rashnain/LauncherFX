e9820fbf2211dd08fa5ca3e78da7818f127cc422
Rashnain <51191602+Rashnain@users.noreply.github.com>
2022-11-05 17:01:53 +0100

    Add comments to PROFILE_TYPE

--- src/main/java/com/github/rashnain/launcherfx/PROFILE_TYPE.java
+++ src/main/java/com/github/rashnain/launcherfx/PROFILE_TYPE.java
@@ -1,8 +1,30 @@
 package main.java.com.github.rashnain.launcherfx;
 
+/**
+ * Profile types that can be used
+ */
 public enum PROFILE_TYPE {
-	CUSTOM, LATEST_RELEASE, LATEST_SNAPSHOT;
+	/**
+	 * Profile bound to the actual latest release version
+	 */
+	LATEST_RELEASE,
 
+	/**
+	 * Profile bound to the actual latest snapshot version
+	 */
+	LATEST_SNAPSHOT,
+
+	/**
+	 * Profile that can use any version
+	 */
+	CUSTOM;
+
+	/**
+	 * Return the string corresponding to the given type<br>
+	 * Ex: LATEST_RELEASE -> "latest-release"
+	 * @param type The given type
+	 * @return The corresponding string or "custom"
+	 */
 	public static String getAsString(PROFILE_TYPE type) {
 		switch (type) {
 		case LATEST_RELEASE:
@@ -14,6 +36,12 @@ public enum PROFILE_TYPE {
 		}
 	}
 
+	/**
+	 * Return the enum corresponding to the given type<br>
+	 * Ex: "latest-release" -> LATEST_RELEASE
+	 * @param type The given type
+	 * @return The corresponding enum or CUSTOM
+	 */
 	public static PROFILE_TYPE getAsType(String type) {
 		switch (type) {
 		case ("latest-release"):
