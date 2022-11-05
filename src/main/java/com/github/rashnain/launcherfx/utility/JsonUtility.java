3fe8d08cc733bae80fdd19bc37252c50123d3bae
Rashnain <51191602+Rashnain@users.noreply.github.com>
2022-11-05 22:07:25 +0100

    Add comments to JsonUtility

--- src/main/java/com/github/rashnain/launcherfx/utility/JsonUtility.java
+++ src/main/java/com/github/rashnain/launcherfx/utility/JsonUtility.java
@@ -13,51 +13,66 @@ import com.google.gson.JsonParser;
 
 public class JsonUtility {
 
+	/**
+	 * Gson instance with pretty printing and disabled HTML escaping (for keeping as is "%")
+	 */
 	private static Gson gson = new GsonBuilder().setPrettyPrinting().disableHtmlEscaping().create();
 
-	public static JsonObject load(String path) throws IOException {
-		FileInputStream in = new FileInputStream(path);
-		StringBuilder sb = new StringBuilder();
-		
-		byte[] buffer = new byte[4048];
-		byte[] subBuffer;
-		int sizeBuffer;
+	/**
+	 * Returns a JsonObject corresponding to the content of the specified file
+	 * @param path Path to the json file
+	 * @return JsonObject or null if the file is not found
+	 */
+	public static JsonObject load(String path) {
+		try {
+			FileInputStream in = new FileInputStream(path);
+			StringBuilder sb = new StringBuilder();
+	
+			byte[] buffer = new byte[4048];
+			byte[] subBuffer;
+			int sizeBuffer;
 
-		while ((sizeBuffer=in.read(buffer)) > 0) {
-			if (sizeBuffer != buffer.length) {
-				subBuffer = new byte[sizeBuffer];
-				for (int i = 0; i < sizeBuffer; i++) { 
-					subBuffer[i] = buffer[i];
+			while ((sizeBuffer=in.read(buffer)) > 0) {
+				if (sizeBuffer != buffer.length) {
+					// Without this, "0" bytes are translated to " " and cause bugs
+					subBuffer = new byte[sizeBuffer];
+					for (int i = 0; i < sizeBuffer; i++) {
+						subBuffer[i] = buffer[i];
+					}
+					sb.append(new String(subBuffer));
+				} else {
+					sb.append(new String(buffer));
 				}
-				sb.append(new String(subBuffer));
-			} else {
-				sb.append(new String(buffer));
+				buffer = new byte[1024];
 			}
-			buffer = new byte[1024];
+	
+			in.close();
+	
+			return JsonParser.parseString(sb.toString()).getAsJsonObject();
+		} catch (IOException e) {
+			return null;
 		}
-		
-		in.close();
-		
-		return JsonParser.parseString(sb.toString()).getAsJsonObject();
 	}
 
+	/**
+	 * Save a JsonElement to a file
+	 * @param path the file where the JsonElement will be saved
+	 * @param json the JsonElement to save
+	 * @return false if an IOException occurred, true otherwise
+	 */
 	public static boolean save(String path, JsonElement json) {
 		try {
-			System.out.println("Saving settings into launcher_profiles.json.");
-			
 			File file = new File(path);
 			file.createNewFile();
+
 			FileOutputStream out = new FileOutputStream(file);
 			out.write(gson.toJson(json).getBytes());
 			out.close();
-			
-			System.out.println("Saved settings.");
+
 			return true;
-		} catch (Exception e) {
-			System.out.println("Couldn't save profile");
+		} catch (IOException e) {
 			e.printStackTrace();
 			return false;
-//			Runtime.getRuntime().exit(1);
 		}
 	}
 }
