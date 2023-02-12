b295c644721ce1d1cbfd0eb946f5c84e7d18f843
Rashnain <51191602+Rashnain@users.noreply.github.com>
2023-02-13 00:47:26 +0100

    Revert to Java 8

--- src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.github.rashnain.launcherfx {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires com.google.gson;
-
-
-    opens com.github.rashnain.launcherfx.controller to javafx.fxml;
-    exports com.github.rashnain.launcherfx;
-}
